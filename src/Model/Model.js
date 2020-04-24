3651e9de6b39b877fa359f69f9c03eeeb778f25f
mbacefor <mbacefor@gmail.com>
2020-04-24 20:31:06 -0300

    Ajuste do modelo

--- src/Model/Model.js
+++ src/Model/Model.js
@@ -6,8 +6,10 @@ export default class Model {
     this._soundOn = true;
     this._musicOn = true;
     this._bgMusicPlaying = false;
-    this._quiz = [new QuizModel('Marcelo', 'Bezerra', ['de', 'Alcantara', 'Bezerra']),
-    new QuizModel('Liliane', 'Bezerra', ['de', 'Alcantara', 'Bezerra'])
+    this._quiz = [new QuizModel('Qual o ano de criação do SERPRO?', '1964', ['2001', '1985', '1973','1964','1880'])
+                  ,new QuizModel('A qual ministério o SERPRO é vinculado?', 'Ministério da Economia', ['Ministério da Economia', 'Ministério da Justiça', 'Ministério da Educação','Ministério da Informação'])
+                  ,new QuizModel('Quantas regionais tem o SERPRO?', '11', ['5', '10', '11','15','24'])
+                  ,new QuizModel('Qual o significado da sigla SERPRO?', 'Serviço Federal de Processamento de Dados', ['Serviço de Processamento de Dados Federal', 'Serviço Federal de Processamento de Dados', 'Serviço de Processamento de Dados','Secretaria Federal de Processamento de Dados'])
     ];
   }
 
--- src/Scenes/QuizScene.js
+++ src/Scenes/QuizScene.js
@@ -2,10 +2,13 @@ import 'phaser';
 import { Dialog } from 'phaser3-rex-plugins/templates/ui/ui-components.js';
 
 
-
 export default class QuizScene extends Phaser.Scene {
+
   constructor() {
     super('QuizScene')
+    this.quizFase = [];
+    this.numeroTentativas = 0
+    const quantidadePerguntas = 1;
   }
 
   preload() {
@@ -16,27 +19,38 @@ export default class QuizScene extends Phaser.Scene {
     });
   }
 
+  escolhePerguntas() {
+    this.model = this.sys.game.globals.model;
+    var quizSelecao = null;
+    this.model.quiz.forEach(function (quiz) {
+      quizSelecao = quiz;
+    });
+    this.quizFase.push(quizSelecao);
+  }
 
   create() {
 
-    this.model = this.sys.game.globals.model;
-    //var nomes = ['maria', 'josé', 'joão'];
-    this.model.quiz.forEach(function (quiz) {
-      console.log(quiz.pergunta);
-      console.log(quiz.resposta);
-      console.log(quiz.opcoes);
-    });
+    this.add.image(400, 300, 'fundoGame');
+    this.escolhePerguntas();
+    this.createDialog();
+  }
 
-    var dialog1 = this.rexUI.add.dialog(
+  update() { }
+
+  createDialog() {
+
+    var quizModel = this.quizFase[0];
+
+    var dialog = this.rexUI.add.dialog(
       {
         x: 400,
         y: 300,
 
-        background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x3e2723),
+        background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x1565c0),
 
         title: this.rexUI.add.label({
-          background: this.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x1b0000),
-          text: this.add.text(0, 0, 'Pergunta 01', {
+          background: this.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x5e92f3),
+          text: this.add.text(0, 0, 'Pergunta - Selecione uma resposta!', {
             fontSize: '24px'
           }),
           space: {
@@ -47,22 +61,22 @@ export default class QuizScene extends Phaser.Scene {
           }
         }),
 
-        content: this.add.text(0, 0, 'Quantos empregados tem o SERPRO?', {
+        content: this.add.text(0, 0, quizModel.pergunta, {
           fontSize: '24px'
         }),
 
-        choices: [
-          createLabel(this, '1.000'),
-          createLabel(this, '2.000'),
-          createLabel(this, '3.000'),
-          createLabel(this, '12.000')
-        ],
+        choices: [createLabel(this, '1'),
+        createLabel(this, '2'),
+        createLabel(this, '3'),
+        createLabel(this, '4'),
+        createLabel(this, '5'),
+        createLabel(this, '6')]
+        ,
 
         space: {
           title: 25,
           content: 25,
           choice: 15,
-
           left: 25,
           right: 25,
           top: 25,
@@ -73,58 +87,22 @@ export default class QuizScene extends Phaser.Scene {
           content: false,  // Content is a pure text object
         }
       });
-    this.add.existing(dialog1);
 
-    var dialog = this.rexUI.add.dialog(
-      {
-        x: 400,
-        y: 300,
-
-        background: this.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x3e2723),
-
-        title: this.rexUI.add.label({
-          background: this.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x1b0000),
-          text: this.add.text(0, 0, 'Pergunta 01', {
-            fontSize: '24px'
-          }),
-          space: {
-            left: 15,
-            right: 15,
-            top: 10,
-            bottom: 10
-          }
-        }),
-
-        content: this.add.text(0, 0, 'Quantos empregados tem o SERPRO?', {
-          fontSize: '24px'
-        }),
-
-        choices: [
-          createLabel(this, '1.000'),
-          createLabel(this, '2.000'),
-          createLabel(this, '3.000'),
-          createLabel(this, '12.000')
-        ],
-
-        space: {
-          title: 25,
-          content: 25,
-          choice: 15,
-
-          left: 25,
-          right: 25,
-          top: 25,
-          bottom: 25,
-        },
-
-        expand: {
-          content: false,  // Content is a pure text object
-        }
-      })
-      .layout()
-      //.drawBounds(this.add.graphics(), 0xff0000)
-      .popUp(1000);
-
+    var choiceTextArray = quizModel.opcoes;
+    var choices = dialog.getElement('choices');
+    var choiceText = null;
+    for (var i = 0, cnt = choices.length; i < cnt; i++) {
+      choiceText = choiceTextArray[i];
+      if (choiceText != null) {
+        dialog.showChoice(i);
+        choices[i].text = choiceText;
+      } else {
+        dialog.hideChoice(i);
+      }
+    }
+    this.add.existing(dialog);
+    dialog.layout()
+    dialog.popUp(1000);
     this.print = this.add.text(0, 0, '');
     dialog
       .on('button.click', function (button, groupName, index) {
@@ -138,12 +116,12 @@ export default class QuizScene extends Phaser.Scene {
       });
   }
 
-  update() { }
+
 }
 
 var createLabel = function (scene, text, backgroundColor) {
   return scene.rexUI.add.label({
-    background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x6a4f4b),
+    background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x5e92f3),
 
     text: scene.add.text(0, 0, text, {
       fontSize: '24px'
@@ -157,4 +135,3 @@ var createLabel = function (scene, text, backgroundColor) {
     }
   });
 }
-
