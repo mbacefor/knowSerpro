ccebd269068dedb6ae25dfa1d48e4df9d3f724ec
mbacefor <mbacefor@gmail.com>
2020-04-26 18:13:26 -0300

    Alerta de mensagem

--- src/Scenes/QuizScene.js
+++ src/Scenes/QuizScene.js
@@ -9,6 +9,7 @@ export default class QuizScene extends Phaser.Scene {
     this.quizFase = [];
     this.numeroTentativas = 0
     this.quantidadePerguntas = 3;
+    this.dialog = undefined;
   }
 
   preload() {
@@ -53,7 +54,7 @@ export default class QuizScene extends Phaser.Scene {
 
     var quizModel = this.quizFase[this.numeroTentativas];
 
-    var dialog = this.rexUI.add.dialog(
+    this.dialog = this.rexUI.add.dialog(
       {
         x: 400,
         y: 300,
@@ -62,7 +63,7 @@ export default class QuizScene extends Phaser.Scene {
 
         title: this.rexUI.add.label({
           background: this.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x5e92f3),
-          text: this.add.text(0, 0, 'Pergunta - Selecione uma resposta!', {
+          text: this.add.text(0, 0, 'Pergunta:'+(this.numeroTentativas+1)+' - Selecione uma resposta!', {
             fontSize: '24px'
           }),
           space: {
@@ -101,22 +102,22 @@ export default class QuizScene extends Phaser.Scene {
       });
 
     var choiceTextArray = quizModel.opcoes;
-    var choices = dialog.getElement('choices');
+    var choices = this.dialog.getElement('choices');
     var choiceText = null;
     for (var i = 0, cnt = choices.length; i < cnt; i++) {
       choiceText = choiceTextArray[i];
       if (choiceText != null) {
-        dialog.showChoice(i);
+        this.dialog.showChoice(i);
         choices[i].text = choiceText;
       } else {
-        dialog.hideChoice(i);
+        this.dialog.hideChoice(i);
       }
     }
-    this.add.existing(dialog);
-    dialog.layout()
-    dialog.popUp(1000);
+    this.add.existing(this.dialog);
+    this.dialog.layout()
+    this.dialog.popUp(1000);
     this.print = this.add.text(0, 0, '');
-    dialog
+    this.dialog
       .on('button.click', function (button, groupName, index) {
         this.print.text += index + ': ' + button.text + '\n';
         this.verificaReposta(button.text);
@@ -141,7 +142,13 @@ export default class QuizScene extends Phaser.Scene {
       this.print.text += 'Errou \n'
     }
     this.numeroTentativas++;
-    this.createDialog();
+    this.dialog.scaleDownDestroy(100);
+    this.dialog = undefined;
+    if (this.numeroTentativas<this.quantidadePerguntas)
+      this.createDialog();
+    else{
+      Alert(this, 'Alerta', 'Acabou!');
+    }  
   }
 
 
@@ -162,4 +169,115 @@ var createLabel = function (scene, text, backgroundColor) {
       bottom: 10
     }
   });
+}
+
+var CreateAlertDialog = function (scene) {
+  var dialog = scene.rexUI.add.dialog({
+      width: 300,
+      background: scene.rexUI.add.roundRectangle(0, 0, 100, 100, 20, 0x1565c0),
+
+      title: scene.rexUI.add.label({
+          background: scene.rexUI.add.roundRectangle(0, 0, 100, 40, 20, 0x003c8f),
+          text: scene.add.text(0, 0, '', {
+              fontSize: '24px'
+          }),
+          space: {
+              left: 15,
+              right: 15,
+              top: 10,
+              bottom: 10
+          }
+      }),
+
+      content: scene.add.text(0, 0, '', {
+          fontSize: '24px'
+      }),
+
+      actions: [
+          scene.rexUI.add.label({
+              background: scene.rexUI.add.roundRectangle(0, 0, 0, 0, 20, 0x5e92f3),
+
+              text: scene.add.text(0, 0, 'OK', {
+                  fontSize: '24px'
+              }),
+
+              space: {
+                  left: 10,
+                  right: 10,
+                  top: 10,
+                  bottom: 10
+              }
+          })
+      ],
+
+      space: {
+          title: 25,
+          content: 25,
+          action: 15,
+
+          left: 20,
+          right: 20,
+          top: 20,
+          bottom: 20,
+      },
+
+      align: {
+          actions: 'center', // 'center'|'left'|'right'
+      },
+
+      expand: {
+          content: false,  // Content is a pure text object
+      }
+  })
+      .on('button.over', function (button, groupName, index, pointer, event) {
+          button.getElement('background').setStrokeStyle(1, 0xffffff);
+      })
+      .on('button.out', function (button, groupName, index, pointer, event) {
+          button.getElement('background').setStrokeStyle();
+      });
+
+  return dialog;
+}
+
+var SetAlertDialog = function (dialog, title, content) {
+  if (title === undefined) {
+      title = '';
+  }
+  if (content === undefined) {
+      content = '';
+  }
+  dialog.getElement('title').text = title;
+  dialog.getElement('content').text = content;
+  return dialog;
+}
+
+var AlertDialog;
+var Alert = function (scene, title, content, x, y) {
+  if (x === undefined) {
+      x = 400;
+  }
+  if (y === undefined) {
+      y = 300;
+  }
+  if (!AlertDialog) {
+      AlertDialog = CreateAlertDialog(scene)
+  }
+  SetAlertDialog(AlertDialog, title, content);
+  AlertDialog
+      .setPosition(x, y)
+      .setVisible(true)
+      .layout();
+
+  return AlertDialog
+      .moveFromPromise(1000, undefined, '-=400', 'Bounce')
+      .then(function () {
+          return scene.rexUI.waitEvent(AlertDialog, 'button.click');
+      })
+      .then(function () {
+          return AlertDialog.moveToPromise(1000, undefined, '-=400', 'Back');
+      })
+      .then(function () {
+          AlertDialog.setVisible(false);
+          return Promise.resolve();
+      })
 }
